914d5fb84257376d528680c9a75f1da83240dff2
FerMod <tudelaigarcia@gmail.com>
2024-10-21 10:15:23 +0200

    Add product filtering

--- src/app/store/[[...categories]]/page.tsx
+++ src/app/store/[[...categories]]/page.tsx
@@ -1,4 +1,5 @@
 import { ProductsWrapper } from "app/components/store/ProductsWrapper";
+import { getCollectionProducts, getCollections } from "app/services/shopify/collections";
 import { getProducts } from "app/services/shopify/products";
 
 interface CategoryProps {
@@ -9,8 +10,19 @@ interface CategoryProps {
 }
 
 export default async function Category(props: CategoryProps) {
-  const products = await getProducts();
   const { categories } = props.params;
+
+  const collections = await getCollections();
+
+  let products = [];
+  if (categories?.length > 0) {
+    const collectionId = collections.find((collection: any) => collection.handle == categories[0]).id;
+    products = await getCollectionProducts(collectionId);
+
+  } else {
+    products = await getProducts();
+  }
+
   return (
     <ProductsWrapper products={products} />
   );
